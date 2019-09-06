6e87f55c407d49b00664eeb92f2d0273154d485d
Zatelbeeit <breaul@gmail.com>
2019-09-06 21:31:41 +0200

    python test file

---
+++ test.py
@@ -0,0 +1,63 @@
+#for i in range(15):
+ #   print(i)
+#print("*******************")
+
+
+#for i in range(15, 0, -1):
+#    print(i)  
+
+
+
+    #range(15) begin=0 end=15 step=1
+   # range(1, 15) begin=1, end=15, step=1
+   # range(15, 0, 1) begin=15, end=0 step=1
+
+#n=6
+#stars=""
+
+#for i in range(n):
+#    stars = stars + "*"
+#print(stars)
+
+#n=7
+#m=2
+#stars =""
+#for i in range(n):
+#    stars = stars + "*"
+#for i in range(m):
+#    print(stars)
+    
+# n=5
+# stars = ""
+# for i in range (n): 
+#     for j in range(n -i -1):
+#         stars+= " "
+#     for k in range (2*i -1):
+#         stars+= "*"
+#     stars += "\n"    
+# print(stars)
+
+# temp=21
+# if temp > 20:
+#     print("too hot")
+    
+# if temp < 20:
+#     print("too cold")
+
+# if temp == 20:
+#     print("just right")
+
+# for i in range (100):
+#     #print(i)
+#     if  i % 2 == 0:
+#         print(str(i) + " even") # or use (i, "even")
+#     else:
+#         print(str(i) + " odd")
+
+
+for i in range (13):
+    print ("Table de multiplication", i)
+    for j in range (13):
+        print(j,'x', i,'=', j * i)
+
+ 
